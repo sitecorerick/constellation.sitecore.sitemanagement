a35ce4a71b0690d58399f8830cdff30acf28e81a
Richard Cabral <sitecorerick@gmail.com>
2014-04-09 16:58:48 -0400

    Nuget management.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,23 +1,18 @@
-﻿// -----------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="genuine">
-//      Copyright (c) @SitecoreRick. All rights reserved.
-// </copyright>
-// -----------------------------------------------------------------------------
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: Guid("095f06f8-c8b9-4f45-a7e9-10888305e17e")]
 [assembly: AssemblyTitle("Constellation Sitecore SiteManagement")]
 [assembly: AssemblyDescription("A site requisition tool for Sitecore")]
 
-[assembly: AssemblyCompany("Genuine Interactive")]
+[assembly: AssemblyCompany("Richard J. Cabral")]
 [assembly: AssemblyProduct("Sitecore SiteManagement Tool")]
-[assembly: AssemblyCopyright("Genuine Interactive")]
-[assembly: AssemblyTrademark("@SitecoreRick")]
+[assembly: AssemblyCopyright("Copyright 2014")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.5.*")]
+[assembly: AssemblyFileVersion("1.5.*")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("DEBUG")]
